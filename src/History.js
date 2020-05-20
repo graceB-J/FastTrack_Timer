c3108252ee8551091e190d26c630e5357024334b
Alex Kyereboah <atk4kcw@virginia.edu>
2020-05-20 12:19:23 -0400

    Added aggregate stats and History.js

---
+++ src/History.js
@@ -0,0 +1,36 @@
+import React from "react";
+
+const History = ({ history }) => {
+  return (
+    <div>
+      <p>History:</p>
+      {history.map((entry) => {
+        return (
+          <div>
+            <p>
+              Date and Time:
+              {entry.startDate.getFullYear() +
+                "-" +
+                (entry.startDate.getMonth() + 1) +
+                "-" +
+                entry.startDate.getDate() +
+                " " +
+                entry.startDate.getHours() +
+                ":" +
+                entry.startDate.getMinutes() +
+                ":" +
+                entry.startDate.getSeconds()}
+            </p>
+            <p>
+              Length of fast: {Math.floor(entry.fastLength / 3600)}:
+              {(entry.fastLength / 60) % 60}
+            </p>
+          </div>
+        );
+      })}
+      <p>Number of completed fasts: {history.length}</p>
+    </div>
+  );
+};
+
+export default History;
