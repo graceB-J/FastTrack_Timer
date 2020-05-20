87b95e911eb59ddcded7d3d63aef02988d966b65
sft3hy <sft3hy@virginia.edu>
2020-05-20 16:39:33 -0400

    Co-authored-by: K. Josiah Park <KyungjinJPark@gmail.com> Co-authored-by: Alexander Kyereboah <alex.kyereboah@gmail.com> Co-authored-by: graceB-J <gb4yy@virginia.edu>

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import "./Button.js";
 import SelectionArea from "./SelectionArea.js";
 import Timer from "./Timer.js";
 import History from "./History.js";
+import FastSurvey from "./FastSurvey.js"
+
 
 class App extends React.Component {
   constructor(props) {
@@ -43,9 +45,22 @@ class App extends React.Component {
     });
   };
 
+  handleAddSurvey = ({ success, difficulty, AdditionalComments }) => {
+    this.setState((prevState) => {
+      let item = prevState.history.pop();
+      item.success = success;
+      item.difficulty = difficulty;
+      item.AdditionalComments = AdditionalComments;
+      prevState.history.push(item);
+      return prevState;
+    });
+  }
+
+
   render() {
     return (
       <div>
+        <FastSurvey FastSurvey handleAddSurvey={this.handleAddSurvey} />
         <SelectionArea handleAddFast={this.handleAddFast} />
         <Timer
           handleFinished={this.handleFinished}
--- src/History.js
+++ src/History.js
@@ -25,6 +25,9 @@ const History = ({ history }) => {
               Length of fast: {Math.floor(entry.fastLength / 3600)}:
               {(entry.fastLength / 60) % 60}
             </p>
+            <p>
+              additional comments: {entry.difficulty}
+            </p>
           </div>
         );
       })}
