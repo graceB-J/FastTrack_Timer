189f06dd9cf13f98715a9fac767665621365f940
Alex Kyereboah <atk4kcw@virginia.edu>
2020-05-20 11:55:27 -0400

    Implemented history display after timer runs down

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
-import React from 'react';
-import './App.css';
-import './Button.js';
-import SelectionArea from './SelectionArea.js';
-import Timer from './Timer.js';
+import React from "react";
+import "./App.css";
+import "./Button.js";
+import SelectionArea from "./SelectionArea.js";
+import Timer from "./Timer.js";
+import History from "./History.js";
 
 class App extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
-      history: []
+      history: [
+        {
+          startDate: new Date(),
+          fastLength: 90 * 60,
+        },
+      ],
     };
   }
-  handleAddFast = length => {
+
+  handleAddFast = (length) => {
     //Tell timer the correct length based on user input
     if (length < 0) {
       length = 0;
@@ -20,19 +27,25 @@ class App extends React.Component {
     }
     this.state = length;
     console.log(length);
-  }
+  };
 
-  // handleFinished = fastInfo => {
-  //   this.setState(prevState => {{
-  //     history: prevState.concat(fastInfo)
-  //   }})
-  // }
+  handleFinished = (fastInfo) => {
+    this.setState((prevState) => {
+      return {
+        history: prevState.history.concat(fastInfo),
+      };
+    });
+  };
 
   render() {
     return (
       <div>
         <SelectionArea handleAddFast={this.handleAddFast} />
-        <Timer totalFastTime={16 * 60 * 60} />
+        <Timer
+          handleFinished={this.handleFinished}
+          totalFastTime={16 * 60 * 60 - 16 * 60 * 60 + 5}
+        />
+        <History history={this.state.history} />
       </div>
     );
   }
--- src/Timer.js
+++ src/Timer.js
@@ -1,83 +1,91 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
-const Timer = ({ totalFastTime }) => {
-    const [startTime, setStartTime] = useState(-1);
-    const [timeLeft, setTimeLeft] = useState(totalFastTime);
-    const [fasting, setFasting] = useState(true);
-    const [totalTime, setTotalTime] = useState(totalFastTime)
+const Timer = ({ handleFinished, totalFastTime }) => {
+  const [startDate, setStartDate] = useState(null);
+  const [startTime, setStartTime] = useState(-1);
+  const [timeLeft, setTimeLeft] = useState(totalFastTime);
+  const [fasting, setFasting] = useState(true);
+  const [totalTime, setTotalTime] = useState(totalFastTime);
 
-    useEffect(() => {
-        if (startTime !== -1) {
-            setTimeLeft(totalTime - ((Date.now() / 1000) - startTime ));
-            const interval = setInterval(() => {
-                setTimeLeft(totalTime - ((Date.now() / 1000) - startTime ));
-                if (totalTime - ((Date.now() / 1000) - startTime ) < 1) {
-                    if (fasting) {
-                        setFasting(false);
-                        setTotalTime((24 * 60 * 60) - totalTime);
-                        setStartTime(Date.now() / 1000);
-                    } else {
-                        clearInterval(interval);
-                    }
-                }
-            }, 500);
-            return () => clearInterval(interval);
-        } else { 
-            setTimeLeft(totalTime);
+  useEffect(() => {
+    if (startTime !== -1) {
+      setTimeLeft(totalTime - (Date.now() / 1000 - startTime));
+      const interval = setInterval(() => {
+        setTimeLeft(totalTime - (Date.now() / 1000 - startTime));
+        if (totalTime - (Date.now() / 1000 - startTime) < 1) {
+          if (fasting) {
+            handleFinished({
+              startDate: startDate,
+              fastLength: totalFastTime,
+            });
+            setFasting(false);
+            setTotalTime(24 * 60 * 60 - totalTime);
+            setStartTime(Date.now() / 1000);
+          } else {
+            clearInterval(interval);
+          }
         }
-    }, [startTime])
-
-    const handleStart = () => {
-        const start = document.getElementById("start");
-        start.disabled = true;
-        setStartTime(Date.now() / 1000);
+      }, 500);
+      return () => clearInterval(interval);
+    } else {
+      setTimeLeft(totalTime);
     }
+  }, [startTime]);
 
-    const handleCancel = () => {
-        const start = document.getElementById("start");
-        start.disabled = false;
-        setTotalTime(totalFastTime);
-        setStartTime(-1);
-        setFasting(true);
-    }
-    
+  const handleStart = () => {
+    let today = new Date();
+    setStartDate(today);
+    const start = document.getElementById("start");
+    start.disabled = true;
+    setStartTime(Date.now() / 1000);
+  };
 
-    return (
-        <div>
-            {fasting && <p>Fasting Period</p>}
-            {!fasting && <p>Eating Period</p>}
-            <Countdown
-                timeLeft={timeLeft} />
-            <TimerButton
-                handleStart={handleStart}
-                handleCancel={handleCancel} />
-            <ProgressBar
-                totalTime={totalTime}
-                timeLeft={timeLeft} />
-        </div>
-    )
-}
+  const handleCancel = () => {
+    const start = document.getElementById("start");
+    start.disabled = false;
+    setTotalTime(totalFastTime);
+    setStartTime(-1);
+    setFasting(true);
+  };
+
+  return (
+    <div>
+      {fasting && <p>Fasting Period</p>}
+      {!fasting && <p>Eating Period</p>}
+      <Countdown timeLeft={timeLeft} />
+      <TimerButton handleStart={handleStart} handleCancel={handleCancel} />
+      <ProgressBar totalTime={totalTime} timeLeft={timeLeft} />
+    </div>
+  );
+};
 
 const Countdown = ({ timeLeft }) => {
-    return (
-        <p>{Math.floor(timeLeft / (60 * 60))}:{Math.floor(timeLeft / 60 % 60)}:{Math.floor(timeLeft % 60)}</p>
-    )
-}
+  return (
+    <p>
+      {Math.floor(timeLeft / (60 * 60))}:{Math.floor((timeLeft / 60) % 60)}:
+      {Math.floor(timeLeft % 60)}
+    </p>
+  );
+};
 
 const TimerButton = ({ handleStart, handleCancel }) => {
-    //Start + Stop/Cancel controls for timer
-    return (
-        <div>
-            <button id="start" onClick={handleStart}>Start</button>
-            <button onClick={handleCancel}>Cancel</button>
-        </div>
-    )
-}
+  //Start + Stop/Cancel controls for timer
+  return (
+    <div>
+      <button id="start" onClick={handleStart}>
+        Start
+      </button>
+      <button onClick={handleCancel}>Cancel</button>
+    </div>
+  );
+};
 
 const ProgressBar = ({ totalTime, timeLeft }) => {
-    return (
-    <div><p>{timeLeft / totalTime * 100}%</p></div>
-    )
-}
+  return (
+    <div>
+      <p>{(timeLeft / totalTime) * 100}%</p>
+    </div>
+  );
+};
 
-export default Timer
+export default Timer;
